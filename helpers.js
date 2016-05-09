5f28b83deebb5d9a6dbf65148e48eddf37e43e22
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-05-09 16:51:44 -0400

    vestar_template 1.0  /helpers.js

---
+++ helpers.js
@@ -0,0 +1,14 @@
+function init(){
+    if ($('#instafeed').length > 0) {
+        var feed = new Instafeed({
+            get: 'user',
+            userId: '1285623815',
+            clientId: 'da5cf03899eb49a496424d9a76bafa0d',
+            template: '<a class="ig-image" target=_blank href="{{link}}" title="{{caption}}" ><img src="{{image}}" /></a>',
+            limit : 10
+
+        });
+    
+        feed.run();
+    }
+}

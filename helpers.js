65984199890270af892ba88b0055752030fcf527
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-05-09 16:54:18 -0400

    vestar_template 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -2,7 +2,7 @@ function init(e){
     if ($('#instafeed').length > 0) {
         var feed = new Instafeed({
             get: 'user',
-            userId: '1285623815',
+            userId: '196558634',
             clientId: 'da5cf03899eb49a496424d9a76bafa0d',
             template: '<a class="ig-image" target=_blank href="{{link}}" title="{{caption}}" ><img src="{{image}}" /></a>',
             limit : 10

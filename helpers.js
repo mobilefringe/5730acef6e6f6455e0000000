a421b8843affea5cd37cc776cf09a118d4dc3d17
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-05-09 16:54:28 -0400

    vestar_template 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -4,7 +4,7 @@ function init(e){
             get: 'user',
             userId: '196558634',
             clientId: 'da5cf03899eb49a496424d9a76bafa0d',
-            template: '<a class="ig-image" target=_blank href="{{link}}" title="{{caption}}" ><img src="{{image}}" /></a>',
+            template: '<a class="ig_image" target=_blank href="{{link}}" title="{{caption}}" ><img src="{{image}}" /></a>',
             limit : 10
 
         });

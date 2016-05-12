eca1ffa6043253d6ca7ccf369c94c016dea768f3
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-05-12 13:27:03 -0400

    vestar_template 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -1,4 +1,6 @@
 function init(e){
+    $('<div class="modal-backdrop custom_backdrop"><img src="http://kodekloud.s3.amazonaws.com/sites/554a79236e6f64713f000000/69e8cd982124dc73de1f5a67a627ee75/loading.gif" class="" alt=""></div>').appendTo(document.body);
+    
     if ($('#instafeed').length > 0) {
         var feed = new Instafeed({
             get: 'user',

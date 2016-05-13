aae0b3442365b3714e503ddc027da8a899d3516c
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-05-13 14:24:53 -0400

    vestar_template 1.0  /site.js

--- site.js
+++ site.js
@@ -1,4 +1,4 @@
 var site_json = {
-                    "name" : "The District at Green Valley Ranch"
+                    "name" : "The District at Green Valley Ranch",
                     "default_image" : "http://assets.codecloudapp.com/sites/5730acef6e6f6455e0000000/image/png/1462909257000/favicon_vestar.png"
                 }

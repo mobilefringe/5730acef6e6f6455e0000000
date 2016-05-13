b89fcbd2e98ed8f566965be22f8822774e0f83c9
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-05-13 14:21:29 -0400

    vestar_template 1.0  /application.js

--- application.js
+++ application.js
@@ -128,7 +128,7 @@ function renderStoreDetails(container, template, collection){
     item_list.push(collection);
     $.each( item_list , function( key, val ) {
         if ((val.store_front_url).indexOf('missing.png') > -1){
-            val.alt_store_front_url = ;
+            val.alt_store_front_url = "";
         } else {
             val.alt_store_front_url = getImageURL(val.store_front_url); 
         }

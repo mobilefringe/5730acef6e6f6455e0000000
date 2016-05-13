dfe3306db10b4cd136ee5a806206676af9ee2319
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-05-13 12:24:09 -0400

    vestar_template 1.0  /application.js

--- application.js
+++ application.js
@@ -107,7 +107,17 @@ function renderStoreList(container, template, collection, type, starter, breaker
     $(container).html(item_rendered.join(''));
 }
 
-
+function renderGeneral(container, template, collection){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    Mustache.parse(template_html); 
+    $.each( collection , function( key, val ) {
+        var repo_rendered = Mustache.render(template_html,val);
+        item_rendered.push(repo_rendered);
+    });
+    $(container).html(item_rendered.join(''));
+}
 
 
 

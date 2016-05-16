e19c30fca072516e6548c3dda7a7dd57c7ac4135
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-05-16 10:45:51 -0400

    vestar_template 1.0  /application.js

--- application.js
+++ application.js
@@ -280,7 +280,7 @@ function renderJobs(container, template, collection){
         if (start.toDateString() == end.toDateString()) {
             val.dates = (get_month(start.getMonth()))+" "+(start.getDate());    
         } else {
-            val.dates = "Starts " + (get_month(start.getMonth()))+" "+(start.getDate())+" - Ends "+get_month(end.getMonth())+" "+end.getDate();    
+            val.dates =  val.job_type + " " + (get_month(start.getMonth()))+" "+(start.getDate())+" - Ends "+get_month(end.getMonth())+" "+end.getDate();    
         }
         var rendered = Mustache.render(template_html,val);
         item_rendered.push(rendered);

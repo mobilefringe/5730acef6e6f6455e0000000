16311ae7ff9942fb80612faf1bbd59fc57b152b3
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-05-19 12:21:59 -0400

    vestar_template 1.0  /application.js

--- application.js
+++ application.js
@@ -499,7 +499,31 @@ function renderJobDetails(container, template, collection){
     $(container).html(item_rendered.join(''));
 }
 
-
+function renderHours(container, template, collection){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    Mustache.parse(template_html);   // optional, speeds up future uses
+    item_list.push(collection);    
+    $.each( item_list , function( key, val ) {
+        val.day = get_day(val.day_of_week);
+        var d = new Date();
+        val.month = get_month(d.getMonth());
+        val.weekday = addZero(d.getDate());
+        if (val.open_time && val.close_time && (val.is_closed == false || val.is_closed == null)){
+            var open_time = new Date (val.open_time);
+            var close_time = new Date (val.close_time);
+            val.open_time = convert_hour(open_time);
+            val.close_time = convert_hour(close_time);    
+            val.h = val.open_time+ " - " + val.close_time;
+        } else {
+            val.h = "Closed";
+        }
+        var rendered = Mustache.render(template_html,val);
+        item_rendered.push(rendered);
+    });
+    $(container).html(item_rendered.join(''));
+}
 
 
 

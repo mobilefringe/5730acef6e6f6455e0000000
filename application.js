d24c715a9caaa70a96aedcec574524b4a263a5b3
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-05-19 12:22:16 -0400

    vestar_template 1.0  /application.js

--- application.js
+++ application.js
@@ -515,9 +515,9 @@ function renderHours(container, template, collection){
             var close_time = new Date (val.close_time);
             val.open_time = convert_hour(open_time);
             val.close_time = convert_hour(close_time);    
-            val.h = val.open_time+ " - " + val.close_time;
+            val.hours = val.open_time+ " - " + val.close_time;
         } else {
-            val.h = "Closed";
+            val.hours = "Closed";
         }
         var rendered = Mustache.render(template_html,val);
         item_rendered.push(rendered);

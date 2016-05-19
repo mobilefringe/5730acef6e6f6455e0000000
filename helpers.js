3003efe3028ec8f331c1ff82167772c9f7846588
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-05-19 12:19:15 -0400

    vestar_template 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -69,7 +69,7 @@ function show_content(){
         }
     }
     
-    renderHomeHours('#home_hours_container', '#home_hours_template', today_hours)
+    renderHomeHours('#hours_container', '#home_hours_template', all_hours)
 }
 
 function show_cat_stores(){

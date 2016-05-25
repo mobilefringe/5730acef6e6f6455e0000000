d03c528ad21a4b8a18ad6dfb8bee6f34776d1e47
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-05-25 13:44:52 -0400

    vestar_template 1.0  /site.js

--- site.js
+++ site.js
@@ -2,5 +2,5 @@ var site_json = {
                     "name" : "The District at Green Valley Ranch",
                     "default_image" : "http://assets.codecloudapp.com/sites/5730acef6e6f6455e0000000/image/jpeg/1463757854000/dgvr_500x500_default.jpg",
                     "time_zone" : "T08:00:00Z",
-                    "instagram" : "da5cf03899eb49a496424d9a76bafa0d"
+                    "instagram" : "196558634"
                 }

95225d1d346004e31b854073cf6f9f9029b6e409
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-05-30 12:47:20 -0400

    vestar_template 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -297,7 +297,7 @@ function init_map(reg, h, w){
 }
 
 
-function show_png_pin(trigger){
+function show_png_pin(trigger, map){
     $(trigger).click(function(e) {
         e.preventDefault()
         var isMobile = ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) );

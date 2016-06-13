48480d0fe7d2fa58db4385d1ee38660e242335ee
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-06-13 13:28:38 -0400

    vestar_template 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -2,16 +2,7 @@ function init(e){
     
     $('<div class="modal-backdrop custom_backdrop"><img src="http://assets.codecloudapp.com/sites/554a79236e6f64713f000000/69e8cd982124dc73de1f5a67a627ee75/loading.gif" class="" alt=""></div>').appendTo(document.body);
     
-    if ($('#instafeed').length > 0) {
-        var feed = new Instafeed({
-            get: 'user',
-            userId: site_json.instagram,
-            clientId: 'da5cf03899eb49a496424d9a76bafa0d',
-            template: '<a class="ig_image" target=_blank href="{{link}}" title="{{caption}}" ><img src="{{image}}" /></a>',
-            limit : 15
-        });
-        feed.run();
-    }
+    
     $('.open_menu').click(function(e){
         $('.mobile_menu').slideToggle();
         $('.open_menu').toggleClass('flip')

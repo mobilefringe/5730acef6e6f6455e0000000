24e6e0216d64e86100febc3515dca001def589fd
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-06-14 13:14:59 -0400

    vestar_template 1.0  /application.js

--- application.js
+++ application.js
@@ -334,7 +334,12 @@ function renderEvents(container, template, collection){
             val.store_name = site_json.name;
             val.image_url = site_json.default_image;
         }
-        console.log(val)
+        if (val.tags.join(',').indexOf('ongoing') > 0){
+            val.hide_dates= "display:none"
+        }
+        else{
+            val.hide_dates= "display:inline"
+        }
         if(val.event_image_url_abs.indexOf('missing.png') > 0){
             val.event_image_url_abs  = site_json.default_image ;
             val.promo_image = "display:none";

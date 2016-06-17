e6799a9b3a27ac541ef856d07eb114c61bb5775d
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-06-17 16:25:11 -0400

    vestar_template 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -251,6 +251,7 @@ function drop_pin(id, map){
     $('#btnZoomIn').click()
     $('#btnZoomIn').click()
     $('#btnZoomIn').click()
+    $('#btnZoomIn').click()
 }
 
 function load_map(reg, store_details, h, w){

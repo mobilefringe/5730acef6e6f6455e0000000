172bbc296cbbff90b6840811af0e5a5c2c4a5f43
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-07-07 12:53:57 -0400

    vestar_template 1.0  /application.js

--- application.js
+++ application.js
@@ -109,12 +109,6 @@ function renderStoreList(container, template, collection, type, starter, breaker
         val.block = current_initial + '-block';
         var rendered = Mustache.render(template_html,val);
         var upper_current_initial = current_initial.toUpperCase();
-        if(starter == '#' && breaker == "#"){
-            if(upper_current_initial > 0){
-                item_rendered.push(rendered);
-            }
-        }
-        
         if (upper_current_initial.charCodeAt(0) < breaker.charCodeAt(0) && upper_current_initial.charCodeAt(0) >= starter.charCodeAt(0)){
             item_rendered.push(rendered);
         }

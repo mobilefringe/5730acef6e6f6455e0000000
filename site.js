f2c705388656b42b322e846aa0b2235b70e5af1b
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-10-20 16:25:50 -0400

    vestar_template 1.2  /site.js

---
+++ site.js
@@ -0,0 +1,7 @@
+var site_json = {
+                    "name" : "The District at Green Valley Ranch",
+                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/57f3bee46e6f643959000000/image/jpeg/1463757854000/dgvr_500x500_default.jpg",
+                    // "time_zone" : "T08:00:00Z",
+                    "time_zone" : moment().tz(getPropertyTimeZone()),
+                    "social_feed": "//costamesa.mallmaverick.com/api/v2/costamesa/social.json"
+                }

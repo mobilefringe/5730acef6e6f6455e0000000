a019e5bf9648edef7aaf707a664426835fcfc2dc
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2017-02-22 12:27:19 -0500

    vestar_template 1.2  /application.js

--- application.js
+++ application.js
@@ -590,7 +590,6 @@ function renderContest(container, template, collection){
     Mustache.parse(template_html);   // optional, speeds up future uses
 
     collection.alt_photo_url = getImageURL(collection.photo_url);
-    collection.property_id = getPropertyID();
     item_list.push(collection);
     collection = [];
     collection = item_list;

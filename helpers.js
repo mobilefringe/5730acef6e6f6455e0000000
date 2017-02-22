ec63f6ea8ef80a79825bdabdd6c7ea04111e2b15
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2017-02-22 13:31:32 -0500

    vestar_template 1.2  /helpers.js

--- helpers.js
+++ helpers.js
@@ -395,7 +395,7 @@ function submit_contest(data) {
     contest_data.first_name = $('#first_name').val();
     contest_data.last_name = $('#last_name').val();
     contest_data.email = $('#email').val();
-    contest_data.phone = $('#phone').val();
+    contest_data.phone = $('#phone_number').val();
     contest_data.notes = $('#company_name').val();
     contest_data.postal_code = $('#team_members').val();
     contest_entry.contest = contest_data;

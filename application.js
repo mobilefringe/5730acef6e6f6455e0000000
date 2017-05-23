7caf1aa36a8b6287da39bd3027fddc466ff40b6d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-05-23 09:28:29 -0400

    vestar_template 1.2  /application.js

--- application.js
+++ application.js
@@ -283,7 +283,6 @@ function renderPromotions(container, template, collection){
         var show_date = moment(val.show_on_web_date);
         var start = moment(val.start_date).tz(getPropertyTimeZone());
         var end = moment(val.end_date).tz(getPropertyTimeZone());
-        console.log(end);
         if (start.format("DMY") == end.format("DMY")){
         	val.dates = start.format("MMM D");
         }

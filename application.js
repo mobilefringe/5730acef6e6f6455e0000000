0b136e6afd7d7395f8847ed3f19ad30362b9881d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-06 10:42:44 -0500

    vestar_template 1.2  /application.js

--- application.js
+++ application.js
@@ -85,6 +85,7 @@ function renderStoreList(container, template, collection, type, starter, breaker
         }
         //var categories = getStoreCategories();
         var current_initial = val.name[0];
+        if(val.categories != null && val.categories != undefined)
         val.cat_list = val.categories.join(',')
         if(store_initial.toLowerCase() == current_initial.toLowerCase()){
             val.data_initial = current_initial;

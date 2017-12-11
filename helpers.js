44ab2e523f62ba9a01f0136ea41d0618ff705481
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-11 11:43:11 -0500

    vestar_template 1.2  /helpers.js

--- helpers.js
+++ helpers.js
@@ -35,6 +35,10 @@ function init(e){
             }
         );
     });
+    
+    //dynamically changing copyright year
+    var current_year = moment().year();
+    $("#current_year").text(current_year)
 }
 
 

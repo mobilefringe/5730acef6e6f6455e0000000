b44276f3c9a6122accd3dcd441839bdc4248cdc0
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2020-04-09 15:38:06 -0400

    vestar_template 1.3  /application.js

--- application.js
+++ application.js
@@ -392,6 +392,7 @@ function renderEvents(container, template, collection){
         // }
         
         var show_date = moment(val.show_on_web_date);
+        console.log('getPropertyTimeZone()', getPropertyTimeZone())
         var start = moment(val.start_date).tz(getPropertyTimeZone());
         var end = moment(val.end_date).tz(getPropertyTimeZone());
         if ((start && end) && (start.format("DMY") == end.format("DMY"))){
